bdef654d1a26f264df2ae33d42912ba60618396a
Ignatius Reza <lyoneil.de.sire@gmail.com>
2026-08-28 08:39:42 +0200

    Update driver configs
    - Add back base coverage
    - Turn on isolatedModules to address TS151002
    - Trn on 2 more eslint rule around unsafe type

--- packages/config/drivers/jest.config.js
+++ packages/config/drivers/jest.config.js
@@ -1,6 +1,14 @@
 export default {
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js}'],
   coveragePathIgnorePatterns: ['<rootDir>/src/lib/Simulator.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 
   clearMocks: true,
   restoreMocks: true,
